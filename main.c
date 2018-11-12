054eeecb205065332cc8fe4789da54794b074611
Yosh Todo <je3hcz@gmail.com>
2018-11-13 05:30:55 +0900

    ファイル削除

--- main.c
+++ main.c
@@ -4,40 +4,29 @@ char    *SERIALPORT     = "/dev/ttyAMA0";
 
 int main(int argc, char *argv[])
 {
+	int 	num;				// 返り値のi を受ける（件数）
+	int	fd;
+	int	i = 0;
+	char	command[32] = {'\0'};
+
 	/* 現在利用可能なリピータリストの取得*/
-	int num;				// 返り値のi を受ける（件数）
 	num = getlinkdata();
 
 	/* GPIO シリアルポートのオープン*/
-	int	fd;
 	fd = openport(SERIALPORT, BAUDRATE);
 
-	/* test */
-	int	i = 0;
+	/* メインスクリーンの初期設定 */
+	sendcmd("dim=100");
+	sendcmd("page MAIN");
+	sendcmd("t0.xcen=\"Left\"");
+	sendcmd("t0.txt=\"LINK TO :         \"");
+	sprintf(command, "t2.txt=\"Read %d RPTs\"", num);
+	sendcmd(command);
+
+	/* 1ページ分21ヶ所分を表示 */
 	for (i = 0; i < num; i++) {
-		printf("%2d : %s %s %s\n", i + 1, linkdata[i].call, linkdata[i].addr, linkdata[i].port);
-	}
-
-	/* リピータリストの表示*/
-	i 	= 0;
-	int	j = 0;
-	char	command[9];
-	char	pagenum[9];
-
-	/* 明るさを100%に設定 */
-	sendcmd("dim=50");
-
-
-	/* 3ページ分を切り替える */
-	for (j = 0; j < 3; j++) {
-		sprintf(pagenum, "page %d", j + 1);
-		sendcmd(pagenum);
-
-		/* 1ページ分21ヶ所分を表示 */
-		for (i = 0; i < 21; i++) {
-			sprintf(command, "t%d.txt=\"%s\"", i + 100, linkdata[i + (j * 21)].call);
-			sendcmd(command);
-		}
+		sprintf(command, "MAIN.va%d.txt=\"%s\"", i, linkdata[i].call);
+		sendcmd(command);
 	}
 
 	/* GPIO シリアルポートのクローズ*/
