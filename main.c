6bbc8d2ff2b2c41e31256581ae2176ef30848fbd
Yosh Todo <je3hcz@gmail.com>
2018-11-26 19:50:14 +0900

    リピータ選択時メイン画面に戻らないケースを修正

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ int main(int argc, char *argv[])
 	int	arraycount;
 	int	fd;
 	int	i;
+	int	flag;
 	char	command[32] 	= {'\0'};
 	char	statpre[32] 	= {'\0'};
 	char	rptcallpre[32] 	= {'\0'};
 	char    *SERIALPORT 	= "/dev/ttyAMA0";
 	char	concall[8]	= {'\0'};
 	char	concallpre[8]	= {'\0'};
+
 
 	/* 現在利用可能なリピータリストの取得*/
 	num = getlinkdata();
@@ -23,6 +25,8 @@ int main(int argc, char *argv[])
 	/* メインスクリーンの初期設定 */
 	sendcmd("dim=50");
 	sendcmd("page MAIN");
+	sendcmd("t1.txt=\"LINK TO : NONE\"");
+	sendcmd("link.txt=\"LINK TO : NONE\"");
 
 	/* 読み込んだリピータの総数を表示 */
 	sprintf(command, "MAIN.stat1.txt=\"Read %d Repeaters\"", num);
@@ -42,26 +46,48 @@ int main(int argc, char *argv[])
 		 * 受信処理
 		 */
 
+		/* タッチパネルのデータを読み込む */
 		recvdata(concall);
-		concall[8] = '\0';
-		if ((strlen(concall) > 0) && (strncmp(concall, concallpre, 8) != 0))  {
+		if ((strlen(concall) > 3) && (strncmp(concall, concallpre, 8) != 0))  {
+
+			/* 現在の返り値を保存 */
 			strncpy(concallpre, concall, 8);
 
-			/* 指定リピータに接続する */
-			i = 0;
-			num = getlinkdata();
-			for (i = 0; i < num; i++) {
-				if (strncmp(linkdata[i].call, concall, 8) == 0) {
+			/* コマンドをスイッチに振り分ける */
+			if (strncmp(concall, "Restart", 7) == 0) flag = 1;
+			if (strncmp(concall, "Reboot",  6) == 0) flag = 2;
+			if (strncmp(concall, "Shutdown",8) == 0) flag = 3;
+			switch (flag) {
+			case 1:
+				sprintf(command, "systemctl restart nextion.service");
+				sendcmd("page MAIN");
+				system(command);
+				break;
+			case 2:
+				sprintf(command, "reboot");
+				sendcmd("page MAIN");
+				system(command);
+				break;
+			case 3:
+				sprintf(command, "shutdown -h now");
+				sendcmd("page MAIN");
+				system(command);
+				break;
+			default:
+				/* 指定リピータに接続する */
+				i = 0;
+				num = getlinkdata();
+				for (i = 0; i < num; i++) {
+					if (strncmp(linkdata[i].call, concall, 8) == 0) {
+						/* 現在稼働中のdmonitor を止める */
+						system("killall -q -s 9 dmonitor");
 
-					/* 現在稼働中のdmonitor を止める */
-					system("killall -q -s 9 dmonitor");
-
-					/* 接続コマンドの実行 */
-					sprintf (command, "sudo dmonitor %s %s '%s'", linkdata[i].addr, linkdata[i].port, linkdata[i].call);
-	printf("%s\n", command);
-					system(command);
-					sendcmd("page MAIN");
-					break;
+						/* 接続コマンドの実行 */
+						sprintf(command, "dmonitor %s %s '%s'", linkdata[i].addr, linkdata[i].port, linkdata[i].call);
+						sendcmd("page MAIN");
+						system(command);
+						break;
+					}
 				}
 			}
 		}
